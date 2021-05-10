69ef3d2dd4f0a2c958176279c263292dd77b1cb8
svetlichnyi-ns <80046790+svetlichnyi-ns@users.noreply.github.com>
2021-05-10 19:43:10 +0300

    Update Hack a function.c

--- Hack a function.c
+++ Hack a function.c
@@ -16,34 +16,34 @@ void honest_func2() {
 
 int hacker_func() {
   void * add1 = &honest_func1; // в переменную add1 типа void * кладём адрес функции honest_func1
-	void * add2 = &honest_func2; // в переменную add2 типа void * кладём адрес функции honest_func2
-	void * add1_2 = &honest_func1 + 1; // в переменную add1_2 типа void * кладём адрес второго по счёту байта, относящегося к коду функции honest_func1
-	size_t page_size = sysconf(_SC_PAGESIZE); // в переменную page_size типа size_t кладём размер страницы в байтах
-	int res = mprotect(add1 - (unsigned long) add1 % page_size, page_size, PROT_READ | PROT_WRITE | PROT_EXEC); // фдрес add1 должен быть выровнен по границе страницы
+  void * add2 = &honest_func2; // в переменную add2 типа void * кладём адрес функции honest_func2
+  void * add1_2 = &honest_func1 + 1; // в переменную add1_2 типа void * кладём адрес второго по счёту байта, относящегося к коду функции honest_func1
+  size_t page_size = sysconf(_SC_PAGESIZE); // в переменную page_size типа size_t кладём размер страницы в байтах
+  int res = mprotect(add1 - (unsigned long) add1 % page_size, page_size, PROT_READ | PROT_WRITE | PROT_EXEC); // адрес add1 должен быть выровнен по границе страницы
   if (res == -1) {
     printf("Memory access violation\n");
     printf("Code error: %d\n", errno);
     return errno;
   } // обработка ошибок при вызове функции mprotect
-	* (char*) add1 = 0xeb; // 0xeb -- операционный код безусловного перехода jmp
-	* (char*) add1_2 = (char*) &honest_func2 - ((char*) &honest_func1 + 2); // вычисление смещения
-	return 0;
+  *(char*) add1 = 0xeb; // 0xeb -- операционный код безусловного перехода jmp
+  *(char*) add1_2 = (char*) &honest_func2 - ((char*) &honest_func1 + 2); // вычисление смещения
+  return 0;
 }
 
 int main() {
   printf("The results, given by functions before hacking:\n");	
-	printf("honest_func1: ");
+  printf("honest_func1: ");
   honest_func1();
   printf("honest_func2: ");
-	honest_func2();
-	int result = hacker_func();
+  honest_func2();
+  int result = hacker_func();
   if (result != 0) {
     return errno;
   }
-	printf("The results, given by functions after hacking:\n");
+  printf("The results, given by functions after hacking:\n");
   printf("honest_func1: ");
-	honest_func1();
+  honest_func1();
   printf("honest_func2: ");
-	honest_func2();
-	return 0;
+  honest_func2();
+  return 0;
 }
